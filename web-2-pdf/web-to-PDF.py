f0b3dc5987676b24ef6c160bda929f270bf307fb
yasin <58345572+YasinBlackhat@users.noreply.github.com>
2020-07-12 09:51:20 +0430

    Ordering

--- web-2-pdf/web-to-PDF.py
+++ web-2-pdf/web-to-PDF.py
@@ -2,12 +2,11 @@
 import requests
 import json
 
-
 in_url = str(input('Enter Your URl Address : ')) # Address => https:\\www.google.com
 loc = str(input('Enter Your Address Location : ')) # Location => C:\\
 
  while in_url == '' and loc == '':
-      print('Humm + Fill in the blanks +')
+      print('Humm    + Fill in the blanks +')
       in_url = str(input('Enter Your URl Address : ')) # URL
       loc = str(input('Enter Your Address Location : ')) # Location
       
@@ -15,6 +14,7 @@ def getting(in_url):
     print('Loading...')
 
     url = 'https://restpack.io/api/html2pdf/v6/convert'
+   
     headers = {
       'Content-Type': 'application/json',
       'x-access-token': 'hHc5xxoTEc5x4EBAjunfG8gQk6g6MEMF7PmB78qjJHYpieLB' # Token or Key 
@@ -25,11 +25,8 @@ def getting(in_url):
     }
 
     response = requests.post(url, headers = headers, params = {}, data = json.dumps(payload))
-
     response.raise_for_status()
-
     k = response.json()
-
     file_loc = k['file']
     r = requests.get(file_loc)
     return r
@@ -39,4 +36,4 @@ loc = loc+'/blachat.pdf'
 with open(loc, 'wb') as f:
     f.write(r.content)
     
-print('Done !!')
+print('Done !! ): ')
