b036719534fc9243dd0c7416393cb8aeaac8f337
yasin <58345572+YasinBlackhat@users.noreply.github.com>
2020-07-13 00:04:15 +0430

    Update web-to-PDF.py

--- web-2-pdf/web-to-PDF.py
+++ web-2-pdf/web-to-PDF.py
@@ -1,39 +1,40 @@
 # Follow me ** https://github.com/YasinBlackhat **
 import requests
 import json
+# import library
 
 in_url = str(input('Enter Your URl Address : ')) # Address => https:\\www.google.com
 loc = str(input('Enter Your Address Location : ')) # Location => C:\\
 
- while in_url == '' and loc == '':
+ while in_url == '' and loc == '': # Check the empty values
       print('Humm    + Fill in the blanks +')
       in_url = str(input('Enter Your URl Address : ')) # URL
       loc = str(input('Enter Your Address Location : ')) # Location
       
-def getting(in_url):
+def getting(in_url): # Function Convert web to PDF
     print('Loading...')
 
-    url = 'https://restpack.io/api/html2pdf/v6/convert'
+    url = 'https://restpack.io/api/html2pdf/v6/convert' # API url
    
-    headers = {
+    headers = { # Header Api
       'Content-Type': 'application/json',
       'x-access-token': 'hHc5xxoTEc5x4EBAjunfG8gQk6g6MEMF7PmB78qjJHYpieLB' # Token or Key 
     }
-    payload = {
+    payload = { # Payload Api
       'url': in_url, # URL For cinvert to Pdf
       'json': 'true'
     }
 
-    response = requests.post(url, headers = headers, params = {}, data = json.dumps(payload))
+    response = requests.post(url, headers = headers, params = {}, data = json.dumps(payload)) # (get) request
     response.raise_for_status()
-    k = response.json()
-    file_loc = k['file']
+    k = response.json() # get response Json
+    file_loc = k['file'] 
     r = requests.get(file_loc)
     return r
   
-r = geting(in_url)
+r = geting(in_url) # get file url for Download
 loc = loc+'/blachat.pdf'
 with open(loc, 'wb') as f:
     f.write(r.content)
     
-print('Done !! ): ')
+print('Done !! ): ') # Down Thank You..
